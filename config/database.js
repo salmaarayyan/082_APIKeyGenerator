d5b32d1bcb68322c1809367ba5169818405836e2
salmaarayyan <salmaarayyan@gmail.com>
2025-11-24 01:04:52 +0700

    initialize database configuration with Sequelize

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3307,
+    dialect: 'mysql',
+    logging: false,
+    define: {
+      timestamps: true
+    }
+  }
+);
+
+module.exports = sequelize;
